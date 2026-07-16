ccc1569e21a84bd5669abcb3ca61c3d13cd0893d
Shreya Choudhury <shreyach.161003@gmail.com>
2026-07-17 03:02:43 +0530

    Delete Test driven development and Logging framework directory

--- Test driven development and Logging framework/JUnit Basic Training Exercises/Setting Up JUnit & Writing Basic JUnit Tests/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.junit;
-
-public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-}
--- Test driven development and Logging framework/Mockito exercises/Verifying Interactions/ExternalApi.java
+++
@@ -1,7 +0,0 @@
-package com.mockito;
-
-public interface ExternalApi {
-
-    String getData();
-
-}
